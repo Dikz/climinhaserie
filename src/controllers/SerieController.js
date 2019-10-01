7a1ec4dbfdaa213a61e2c89f8ef97a62f54ed5a0
Dikz <cicerogabriel12@hotmail.com>
2019-10-01 11:28:41 -0300

    Add: Temporadas+Epsodios

--- src/controllers/SerieController.js
+++ src/controllers/SerieController.js
@@ -4,19 +4,19 @@ const chalk = require('chalk')
 const Table = require('cli-table')
 const Spinner = require('cli-spinner').Spinner
 
-let serieTable = new Table({
-    style: {
-        'padding-left': 1,
-        'padding-right': 1
-    }
-})
-
-let spinner = new Spinner('%s Buscando... ')
-spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠏')
-
 class SeriePart {
     constructor () {
-        this.spinner = spinner
+        this.spinner = new Spinner({
+            text: '%s Buscando... ',
+        })
+        this.spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠏')
+
+        this.serieTable = new Table({
+            style: {
+                'padding-left': 1,
+                'padding-right': 1
+            }
+        })
     }
 
     top() {
@@ -25,11 +25,10 @@ class SeriePart {
             .then(res => {
                 let listchoices = [];
                 res.data.results.map(serie => {
-                    let add = {
+                    listchoices.push({
                         value: serie.name,
                         name: serie.title
-                    }
-                    listchoices.push(add)
+                    })
                 })
                 cli.prompt([{
                         type: "list",
@@ -40,26 +39,21 @@ class SeriePart {
                     .then(res => {
                         //console.log(res.serie)
                         this.spinner.start()
-                        axios.get(`https://minhaserieapi.herokuapp.com/serie/${res.serie}`)
+                        axios.get(`https://minhaserieapi.herokuapp.com/series/${res.serie}`)
                             .then(res => {
                                 let serie = res.data;
 
                                 console.log("\n\n  📺 Informações da serie: \n")
-                                //console.log(chalk.hex("#9e82a8").bold("Titulo: "), chalk.white(serie.title))
-                                //console.log(chalk.hex("#9e82a8").bold("Categoria: "), chalk.white(serie.category))
-                                //console.log(chalk.hex("#9e82a8").bold("Lançamento:\n"), chalk.white(serie.debut))
-                                //console.log(chalk.hex("#9e82a8").bold("Visitas: "), chalk.white(`${serie.visits} visitas.`))
-                                //console.log(chalk.hex("#9e82a8").bold("Descrição:\n"), chalk.white(serie.description))
-                                //console.log(chalk.hex("#9e82a8").bold("Link: "), chalk.white(serie.link))
-                                serieTable.push(
+                                this.serieTable.push(
                                     [chalk.hex("#9e82a8").bold("Titulo:"), chalk.white(serie.title)],
                                     [chalk.hex("#9e82a8").bold("Categoria:"), chalk.white(serie.category)],
                                     [chalk.hex("#9e82a8").bold("Lançamento:"), chalk.white(serie.debut)],
-                                    [chalk.hex("#9e82a8").bold("Visitas:"), chalk.white(serie.visits)],
+                                    [chalk.hex("#9e82a8").bold("Temporadas:"), chalk.white(serie.seasons)],
+                                    [chalk.hex("#9e82a8").bold("Epsodios:"), chalk.white(serie.episodes)],
                                     [chalk.hex("#9e82a8").bold("Link:"), chalk.white(serie.link)],
                                     //[serie.description]
                                 )
-                                console.log(serieTable.toString())
+                                console.log(this.serieTable.toString())
                             })
                             .catch(err => {
                                 console.log(err)
